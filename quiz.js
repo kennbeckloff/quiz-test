21c8945df112f356d2e6005caa34d20d95234672
kennbeckloff <kennbeckloff688@gmail.com>
2022-05-15 14:59:25 +0300

    javascript scripts to calculate % and select correct answers and display output added successfully and working

--- quiz.js
+++ quiz.js
@@ -2,3 +2,37 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+// start of  quiz test functionality
+const quizForm = document.querySelector('.form');
+const resultPanel = document.querySelector('#output');
+const Marks = document.querySelector('.marks');
+const testAnswers = ['A', 'A', 'B', 'C', 'C'];
+
+quizForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let userAnswers = [quizForm.qn1.value, quizForm.qn2.value, quizForm.qn3.value, quizForm.qn4.value, quizForm.qn5.value];
+  let score = 0;
+
+  userAnswers.forEach((answer, index) => {
+    if (answer === testAnswers[index]) {
+      score += 20;
+    }
+  });
+
+  let output = 0;
+
+  const counter = setInterval(() => {
+    Marks.textContent = `${output}%`;
+
+    if (output === score) {
+      clearInterval(counter);
+    } else {
+      output++;
+    }
+  }, 10)
+
+  quizForm.reset();
+
+});
+
